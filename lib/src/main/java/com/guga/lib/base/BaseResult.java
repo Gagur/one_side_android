9c88ff74e3d4113ea2a2159747cde079c81c52b4
pingfu.fu <pingfuf.fu@qunar.com>
2016-01-29 19:47:28 +0800

    添加Httpqi请求

---
+++ lib/src/main/java/com/guga/lib/base/BaseResult.java
@@ -0,0 +1,7 @@
+package com.guga.lib.base;
+
+/**
+ * Created by pingfu on 16/1/27.
+ */
+public class BaseResult {
+}
---
+++ lib/src/main/java/com/guga/lib/base/BaseService.java
@@ -0,0 +1,13 @@
+package com.guga.lib.base;
+
+/**
+ * Created by pingfu on 16/1/27.
+ */
+public enum BaseService {
+    OS_CHECK_USER("", BaseResult.class);
+
+
+    BaseService(String s, Class<BaseResult> baseResultClass) {
+
+    }
+}
